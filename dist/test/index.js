e9fa4030ee6fd14f37527c64d542d237f87850c7
weiguo.kong <weiguo.kong@qunar.com>
2018-05-18 18:34:27 +0800

    优化

--- dist/test/index.js
+++ dist/test/index.js
@@ -4,5 +4,12 @@ const MwLoader_1 = require("../src/lib/MwLoader");
 const koa = require("koa");
 const app = new koa();
 const mwl = new MwLoader_1.MwLoader(process.cwd() + '/dist/test/config/middleware', process.cwd() + '/dist/test/middleware');
+const match = async (ctx, next) => {
+    mwl.use(ctx, ['localA'], () => {
+        console.log('controller.action run');
+    });
+    next && next();
+};
+app.use(match);
 mwl.use(app, ['localA']);
 app.listen(3333);
--- test/index.ts
+++ test/index.ts
@@ -2,5 +2,12 @@ import { MwLoader } from '../src/lib/MwLoader';
 import koa = require('koa');
 const app = new koa();
 const mwl = new MwLoader(process.cwd() + '/dist/test/config/middleware', process.cwd() + '/dist/test/middleware');
+const match = async (ctx:Object, next?: Function) => {
+  mwl.use(ctx, ['localA'], () => {
+    console.log('controller.action run');
+  });
+  next && next();
+}
+app.use(match);
 mwl.use(app, ['localA']);
 app.listen(3333);
